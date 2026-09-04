6c002858afc7881075293a302e550ae911200f65
SimYuBo <104333238+SimYuBo@users.noreply.github.com>
2026-09-04 14:50:30 +0800

    Fun Conditions

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("====================")
 print("Welcome here")
 print("My first post!")
 print("====================")
+
+username = "Biggest_Bird"
+bio = "Just a bird trying to make it in the world!"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
