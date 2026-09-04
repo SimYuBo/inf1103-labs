ebb9e455f199aa764e651d80f858a731f6cd892b
SimYuBo <104333238+SimYuBo@users.noreply.github.com>
2026-09-04 14:56:57 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -1,21 +1,9 @@
+username = input("Enter username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
 print("====================")
-print("Welcome here")
-print("My first post!")
-print("====================")
-
-username = "Biggest_Bird"
-bio = "Just a bird trying to make it in the world!"
-followers = 100
-
-followers += 50  # Increment followers by 50
-print("Day 1:", followers)
-
-followers += 20  # Increment followers by 20
-print("Day 2:", followers)
-
-followers -= 10  # Decrement followers by 10
-print("Day 3:", followers)
-
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Username: " + username)
+print("Age: " + age)
+print("Content Category: " + category)
